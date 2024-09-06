4012a5fbc8a112cae1f8ca6d029184f9ef361b7a
larissacoelho21 <larissa673rodrigues@gmail.com>
2024-09-06 16:04:17 -0300

    elaboração da navBar professor

---
+++ PlanneEdu_Front/src/Pages/HomeTeacher.tsx
@@ -0,0 +1,9 @@
+import { NavBarProfessor } from "../Components/NavBar-Professores/navBarProfessor";
+
+export function HomeTeacher() {
+    return (
+        <section className="HomeProfessor">
+            <NavBarProfessor />
+        </section>
+    )
+}
--- PlanneEdu_Front/src/Router.tsx
+++ PlanneEdu_Front/src/Router.tsx
@@ -3,8 +3,10 @@ import { Login } from './Pages/Login';
 import { PasswordReset } from './Pages/PasswordReset';
 import { VerificationEmail } from './Pages/Verification';
 import { CreatePassword } from './Pages/CreatePassword';
+import { NavBarProfessor } from './Components/NavBar-Professores/navBarProfessor';
+import { HomeTeacher } from './Pages/HomeTeacher';
 
-//criando rotas e caminhos
+//criando rotas e caminhos 
 const Router: React.FC = () => {
     return (
        <BrowserRouter>
@@ -13,6 +15,10 @@ const Router: React.FC = () => {
                     <Route path='/redefinicaosenha' element={<PasswordReset />}></Route>
                     <Route path='/verificacaoemail' element={<VerificationEmail />}></Route>
                     <Route path='/novasenha' element={<CreatePassword />}></Route>
+                    <Route path='/homeprofessor' element={<HomeTeacher />}></Route>
+                    <Route path='/navbar' element={<NavBarProfessor />}></Route>
+                    
+
                 </Routes>
         </BrowserRouter>
 
