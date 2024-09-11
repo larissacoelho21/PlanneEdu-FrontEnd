236613e0466476ee6aaca0d738276b04a50f5db2
Larissa <larissa673rodrigues@gmail.com>
2024-09-11 16:50:32 -0300

    realização do dropdown das notificações

--- PlanneEdu_Front/src/Components/NavBar-Professores/navBarProfessor.tsx
+++ PlanneEdu_Front/src/Components/NavBar-Professores/navBarProfessor.tsx
@@ -1,73 +1,98 @@
 /* Images */
 import LogoName from "../../assets/logoname.svg"
-import Notifications from "../../assets/notifications.svg"
+
+/* import Notifications from "../../assets/notifications.svg"
 import Profile from "../../assets/profile.svg"
-import DarkMode from "../../assets/darkmode.svg"
+import DarkMode from "../../assets/darkmode.svg" */
+
+/* Imagens que não foram utilizadas, icones do dropdown */
 /*  import DsIcon from "../../assets/dsIcon.svg"
 import Eletromecanica from "../../assets/eletroIcon.svg"
 import Logistica from "../../assets/logIcon.svg"
 import AdmIcon from "../../assets/folderIcon.svg"
 import Geral from "../../assets/geralIcon.svg" */
 
-/* Icons */
-import { CodeXml } from 'lucide-react';
-import { Settings } from 'lucide-react';
-import { GalleryVerticalEnd } from 'lucide-react';
-import { Folder } from 'lucide-react';
-import { ChartLine } from 'lucide-react';
-import { ChevronDown } from 'lucide-react';
+/* Icons do dropdown */
+import { CodeXml, Settings, GalleryVerticalEnd, Folder, ChartLine, ChevronDown } from 'lucide-react';
 
 /* funções react */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link, NavLink } from "react-router-dom";
-import { /* ReactNode */ ReactNode, useEffect, useRef, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 
 /* css */
 import "../NavBar-Professores/navBarProfessor.css"
+import { faBell, faMoon, faUser } from "@fortawesome/free-solid-svg-icons";
 
+
+/* Criando interface para declarar as props do DropdownItem */
 interface DropdownItemProps {
-    icon: ReactNode;
-    text: string;
-    to: string; /* Link  */
+    icon: ReactNode; /* React Node utilizado para aceitar icone(svg) ou string */
+    text: string; /* nome do link */
+    to: string; /* path para as páginas */
 }
 
+/* props ciradas na Interface */
 function DropdownItem({ icon, text, to }: DropdownItemProps) {
     return (
-        <NavLink to={to} className='dropdownItem'>
-            <i className="icon">{icon}</i>
-            <Link className="menu-a" to="#"> {text} </Link>
+        <NavLink to={to} className='dropdownItem'> {/* usado para navegar as páginas de forma renderizada */}
+            <i className="icon">{icon}</i> {/* imagem */}
+            <Link className="menu-a" to="#"> {text} </Link> {/* Link */}
         </NavLink>
     );
 }
 
+interface DropdownNotificationProps {
+    text: string;
+    secondtext: string;
+    to: string;
+}
+
+function DropdownNotification({ text, secondtext, to }: DropdownNotificationProps) {
+    return (
+        <NavLink to={to} className='dropdownNotification'>
+            <p className="paragrafo">
+                {text}
+            </p>
+            <p className="sub-paragrafo">
+                {secondtext}
+            </p>
+        </NavLink>
+    )
+}
+
 export function NavBarProfessor() {
 
-    const [open, setOpen] = useState(false);
+    /* Criando função para identificarquando o dropdown esta ativo */
+    const [openOne, setOpenOne] = useState(false);
+    const [openTwo, setOpenTwo] = useState(false);
 
     let menuRef = useRef<HTMLDivElement>(null);
+    let notRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        let handler = (e: MouseEvent) => {
+        const handleClickOutside = (e: MouseEvent) => {
             if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
-                setOpen(false);
-                console.log(menuRef.current);
+                setOpenOne(false);
+            }
+            if (notRef.current && !notRef.current.contains(e.target as Node)) {
+                setOpenTwo(false);
             }
         };
 
-        document.addEventListener("mousedown", handler);
-
+        document.addEventListener("mousedown", handleClickOutside);
 
         return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-
-    });
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     return (
         <section className="navbar-prof">
             <nav>
                 <div className="navbarOne">
                     <div className="right-side">
-                        <div className="nav-logo">
+                        <div className="nav-logo"> {/* logo projeto */}
                             <Link to="/homeprofessor"><img src={LogoName} alt="" /></Link>
                         </div>
                     </div>
@@ -77,39 +102,39 @@ export function NavBarProfessor() {
                         <div className="nav-list" ref={menuRef}>
                             <ul className="navbar-nav" >
 
-                                <NavLink
+                                <NavLink /* identificando se o caminho da página selecionada corresponde */
                                     className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
-                                    to="#"> Início 
+                                    to="/homeprofessor"> Início
                                 </NavLink>
 
-                                    {/* < li >
+                                {/* < li >
                                         <Link className="navbar-link" to="/homeprofessor">Ínicio</Link>
                                     </li> */}
 
                                 <li className="nav-dropdown">
-                                    <div className="li-inicio" onClick={() => { setOpen(!open) }}>
-                                        <div className={`navbar-link ${open ? 'selected' : ''}`} >
+                                    <div className="li-inicio" onClick={() => { setOpenOne(!openOne) }}>
+                                        <div className={`navbar-link ${openOne ? 'selected' : ''}`} > {/* Identificando quando estiver clicado (selecionado) aparecer diferente */}
                                             Planos de ensino <ChevronDown />
                                         </div>
                                     </div>
 
                                     <li>
-                                        <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
-                                            <DropdownItem to="/" icon={<CodeXml size={20} />} text={"Desen. de sistemas"}  />
+                                        <div className={`dropdown-menu ${openOne ? 'active' : 'inactive'}`}> {/* menu */}
+                                            <DropdownItem to="/" icon={<CodeXml size={20} />} text={"Desen. de sistemas"} />
                                             <DropdownItem to="/" icon={<Settings size={20} />} text={"Eletromecânica"} />
                                             <DropdownItem to="/" icon={<ChartLine size={20} />} text={"Logística"} />
-                                            <DropdownItem to="/" icon={<Folder size={20}/>} text={"Administração"} />
-                                            <DropdownItem to="/" icon={<GalleryVerticalEnd size={20}/>} text={"Geral"} />
+                                            <DropdownItem to="/" icon={<Folder size={20} />} text={"Administração"} />
+                                            <DropdownItem to="/" icon={<GalleryVerticalEnd size={20} />} text={"Geral"} />
                                         </div>
                                     </li>
 
                                 </li>
 
                                 <NavLink
-                                    className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+                                    className={({ isActive }) => (isActive ? 'navbar-link active last' : 'navbar-link')}
                                     to="/"
                                 >
-                                    Planos de curso 
+                                    Planos de curso
                                 </NavLink>
                                 {/* <li><Link className="navbar-link" to="/">Planos de curso</Link></li> */}
 
@@ -117,16 +142,43 @@ export function NavBarProfessor() {
 
                         </div>
 
-                        <div className="profile">
-                            <div className="notifications">
-                                <img src={Notifications} alt="" />
+                        <div className="Icons">
+                            <div className="icon-list" ref={notRef}>
+                                <ul>
+                                    <li className="notification-dropdown">
+                                        <div className="li-bell" onClick={() => { setOpenTwo(!openTwo) }}>
+                                            <div className="link-bell">
+                                                <FontAwesomeIcon icon={faBell} className="iconsBell" />
+                                            </div>
+                                        </div>
+
+                                        <li>
+                                            <div className={`dropdown-notification ${openTwo ? 'ativo' : 'inactive'}`}>
+                                                <DropdownNotification to="#" text={"O professor Giovani respondeu ao seu comentário."} secondtext={"Toque aqui para visualizar."} />
+                                            </div>
+                                            
+                                        </li>
+
+                                    </li>
+
+                                    <li className="li-notification">
+                                        <div className="secondPart">
+                                            <div className="profile">
+                                                <NavLink to="#" className="profileIcon">
+                                                    <FontAwesomeIcon icon={faUser} className="not-icons" />
+                                                </NavLink>
+                                            </div>
+                                            <div className="darkmode">
+                                                <NavLink to="#" className="darkmodeIcon">
+                                                    <FontAwesomeIcon icon={faMoon} className="not-icons" />
+                                                </NavLink>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                </ul>
                             </div>
-                            <div className="perfil">
-                                <img src={Profile} alt="" />
-                            </div>
-                            <div className="darkMode">
-                                <img src={DarkMode} alt="" />
-                            </div>
+
                         </div>
                     </div>
 
