efbe4e64afec5a73f33a8e4e97640709b0d7e451
laraazx <lara.zxnelato@gmail.com>
2024-09-22 21:38:44 -0300

    Continuando formulário de criação de atividades.

--- PlanneEdu_Front/src/Pages/Teacher/AddActivity.tsx
+++ PlanneEdu_Front/src/Pages/Teacher/AddActivity.tsx
@@ -2,56 +2,51 @@ import { Clock3, Users } from "lucide-react";
 import { SubNavbar } from "../../Components/SubNavbar/SubNavbar";
 import { useState } from "react";
 import "../../Css/Teacher/AddActivity.css";
+import {
+  Multiselect,
+  SelectOption,
+} from "../../Components/Multiselect/Multiselect";
+
+interface InputFieldProps {
+  id: string;
+  label: string;
+  type?: string;
+}
+
+function InputField({ id, label, type = "text" }: InputFieldProps) {
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsFilled(event.target.value !== "");
+  };
+
+  return (
+    <fieldset className={`Fieldset ${isFilled ? "filled" : ""}`}>
+      <label className="label-add" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className="input-add"
+        id={id}
+        type={type}
+        onChange={handleInputChange}
+      />
+    </fieldset>
+  );
+}
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
 
 export function AddActivity() {
-  function InputField({
-    id,
-    label,
-    type = "text",
-    multiline = false, // Adiciona a opção de multiline
-  }: {
-    id: string;
-    label: string;
-    type?: string;
-    multiline?: boolean;
-  }) {
-    const [isFilled, setIsFilled] = useState(false);
 
-    const handleInputChange = (
-      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-      setIsFilled(event.target.value !== "");
-    };
-
-    return (
-      <div className="input-container">
-        {multiline ? (
-          <textarea
-          className="input-add"
-          id={id}
-          onInput={(e) => {
-            const target = e.target as HTMLTextAreaElement;
-            target.style.height = "auto";
-            target.style.height = `${target.scrollHeight}px`; // Ajusta conforme o conteúdo
-          }}
-          rows={1} // Inicialmente com 1 linha
-          style={{ overflow: "hidden" }} // Oculta a barra de rolagem
-        />
-        
-        ) : (
-          <input
-            type={type}
-            className={`input-add ${isFilled ? "filled" : ""}`}
-            id={id}
-            onChange={handleInputChange}
-          />
-        )}
-        <label className={`label-add ${isFilled ? "filled" : ""}`} htmlFor={id}>
-          {label}
-        </label>
-      </div>
-    );
-  }
+  const [value, setValue] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption[]>([options[0]]);
 
   return (
     <section className="add-activity">
@@ -82,20 +77,61 @@ export function AddActivity() {
       <div className="form-addactivity">
         <div className="dates-add">
           <div className="proposed-date">
-            <label htmlFor="" className="label-date">Data Proposta</label>
+            <label htmlFor="" className="label-date">
+              Data Proposta
+            </label>
             <input type="date" />
           </div>
           <div className="delivery-date">
-            <label htmlFor="" className="label-date">Data de Entrega</label>
+            <label htmlFor="" className="label-date">
+              Data de Entrega
+            </label>
             <input type="date" />
           </div>
         </div>
-        <InputField
-          id="description"
-          label="Descrição da Atividade"
-          type="text" 
-          multiline={true} 
-        />
+
+        <div className="add-category">
+          <div className="title-add">
+            <h1>Estratégia de aprendizagem adotada</h1>
+          </div>
+          <div className="buttons-add">
+            <button>Situação Problema</button>
+            <button>Estudo de Caso</button>
+            <button>Projeto</button>
+            <button>Projeto Integrador</button>
+            <button>Pesquisa</button>
+            <button>Mostrar todos</button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="captecbasic">
+            <label className="label-captecbasic">Capacidades Técnicas ou Básicas</label>
+            <Multiselect
+              options={options}
+              value={value}
+              onChange={(o) => setValue(o)}
+              multiple={true}
+            />
+          </div>
+          <div className="capsocio">
+            <label className="label-capsocio">Capacidades Socioemocionais</label>
+            <Multiselect
+              options={options}
+              value={value2}
+              onChange={(o) => setValue2(o)}
+              multiple={true}
+            />
+          </div>
+        </div>
+
+        <div className="contextualization">
+          <InputField id="contextualization" label="Contextualização" type="text" />
+        </div>
+
+        <div className="results-add">
+          <InputField id="contextualization" label="Resultados Esperados" type="text" />
+        </div>
       </div>
     </section>
   );
