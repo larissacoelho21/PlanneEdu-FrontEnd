e72d44348af5ae9a9b8be206921ec0ccf39e4329
Sarah Cruz <sarinha.studiesllz@gmail.com>
2024-09-25 16:49:49 -0300

    Continuação da construção da página referente a criação de planos de ensino

--- PlanneEdu_Front/src/Pages/Teacher/AddPlans.tsx
+++ PlanneEdu_Front/src/Pages/Teacher/AddPlans.tsx
@@ -1,8 +1,10 @@
+/* importações dos icones, css e componentes */
 import "../../Css/Teacher/AddPlans.css";
 import { SubNavbar } from "../../Components/SubNavbar/SubNavbar";
 import { CodeXml } from "lucide-react";
 import { GraduationCap } from "lucide-react";
 import { BookMarked } from "lucide-react";
+import { Plus } from "lucide-react";
 import {
   Multiselect,
   SelectOption,
@@ -18,10 +20,78 @@ const options = [
 ];
 
 export function AddPlans() {
+  /* ======== configurações utilizadas no multiselect ======== */
   const [value, setValue] = useState<SelectOption[]>([options[0]]);
   const [value2, setValue2] = useState<SelectOption[]>([options[0]]);
   const [value3, setValue3] = useState<SelectOption[]>([options[0]]);
   const [value4, setValue4] = useState<SelectOption[]>([options[0]]);
+  const [value5, setValue5] = useState<SelectOption[]>([options[0]]);
+  const [value6, setValue6] = useState<SelectOption[]>([options[0]]);
+
+  /* ======== funções e configurações para a criação da tabela ======== */
+
+  /* definindo a estrutura dos dados da tabela, todos os itens terão uma propriedade de texto (text) e seleção (selectedOption) */
+  type TableData = {
+    text: string;
+    selectedOption: string;
+  };
+
+  /* gerencia o valor inputado no input de texto */
+  const [textInput, setTextInput] = useState<string>("");
+  /* gerencia o valor inputado no select */
+  const [selectedOption, setSelectedOption] = useState<string>("");
+  /* armazena os dados da tabela, é uma array de objetos*/
+  const [tableData, setTableData] = useState<TableData[]>([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+
+  /* array das opções presentes no select */
+  const selectOptions = ["C", "D"];
+
+  const addData = () => {
+    if (textInput && selectedOption) {
+      const newData: TableData = {
+        text: textInput,
+        selectedOption: selectedOption,
+      };
+      setTableData([...tableData, newData]);
+      setTextInput("");
+      setSelectedOption("");
+    }
+  };
+
+  const SaveEdit = () => {
+    if (editingIndex !== null) {
+      const updatedData = [...tableData];
+      updatedData[editingIndex] = {
+        text: textInput,
+        selectedOption: selectedOption,
+      };
+      setTableData(updatedData);
+      setIsEditing(false);
+      setTextInput("");
+      setSelectedOption("");
+      setEditingIndex(null);
+      setShowPopUpAdd(false);
+    }
+  };
+
+  const DeleteData = (index: number) => {
+    const updatedData = tableData.filter((_, i) => i !== index);
+    setTableData(updatedData);
+  };
+
+  const [showPopUpAdd, setShowPopUpAdd] = useState(false);
+  const togglePopUpAdd = () => {
+    setShowPopUpAdd(!showPopUpAdd);
+  };
+
+  const [showPopUpPlan, setShowPopUpPlan] = useState(false);
+  const togglePopUpPlan = () => {
+    setShowPopUpPlan(!showPopUpPlan);
+  };
+
+  /* ======== layout e estrutura da página ======== */
 
   return (
     <main>
@@ -30,17 +100,17 @@ export function AddPlans() {
         <h2>Programação WEB Front-End</h2>
 
         <div className="tag-course">
-          <CodeXml />
+          <CodeXml strokeWidth={1.5} />
           <p>Desenvolvimento de Sistemas</p>
         </div>
 
         <div className="Info">
           <div className="Teacher">
-            <GraduationCap />
+            <GraduationCap strokeWidth={1.5} />
             <p>Arthur Rosa</p>
           </div>
           <div className="Class">
-            <BookMarked />
+            <BookMarked strokeWidth={1.5} />
             <p>Manhã 2023</p>
           </div>
         </div>
@@ -90,7 +160,7 @@ export function AddPlans() {
           <input type="text" name="turma" id="turma" />
         </div>
 
-        <h3>
+        <h3 className="title-section">
           Estratégias para o desenvolvimento da situação de aprendizagem e
           planejamento da intervenção mediadora
         </h3>
@@ -98,11 +168,7 @@ export function AddPlans() {
         <div className="row">
           <div className="inputt">
             <label>Data</label>
-            <input type="number" name="horas" id="horas" />
-          </div>
-          <div className="inputt">
-            <label>N° S.A</label>
-            <input type="number" name="n-sa" id="n-sa" />
+            <input type="date" name="horas" id="horas" />
           </div>
           <div className="inputt">
             <label>Carga Horária</label>
@@ -157,7 +223,7 @@ export function AddPlans() {
           <input type="text" name="pergmedia" id="pergmedia" />
         </div>
 
-        <h3>Estratégias de avaliação de aprendizagem</h3>
+        <h3 className="title-section">Estratégias de avaliação de aprendizagem</h3>
 
         <div className="inputt">
           <label>Instrumentos de Avaliação</label>
@@ -165,12 +231,163 @@ export function AddPlans() {
         </div>
 
         <div className="criterios">
-          <table>
-            <tr>
-              <th>Critérios de Avalição</th>
-              <th>Crítico (C) ou Desejável (D)</th>
-            </tr>
+          <div className="inputt">
+            <label>Critérios de avaliação</label>
+            <input
+              type="text"
+              name="crit-avalia"
+              id="crit-avalia"
+              value={textInput}
+              onChange={(e) => setTextInput(e.target.value)}
+            />
+          </div>
+          <div className="inputt">
+            <label>Crítico (C) ou Desejável (D)</label>
+            <select
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
+              <option value="" disabled></option>
+              {selectOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <button className="add-btn" onClick={addData}>
+            Adicionar à Tabela
+          </button>
+
+          <table className="table-criterios">
+            <thead>
+              <tr>
+                <th>Critérios de avaliação</th>
+                <th>Crítico (C) e Desejável (D)</th>
+                <th>Editar</th>
+                <th>Deletar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((data, index) => (
+                <tr key={index}>
+                  <td>{data.text}</td>
+                  <td>{data.selectedOption}</td>
+                  <td>
+                    <button
+                      className="action-btn"
+                      onClick={() => {
+                        setEditingIndex(index);
+                        setTextInput(data.text);
+                        setSelectedOption(data.selectedOption);
+                        setShowPopUpAdd(true);
+                      }}
+                    >
+                      Editar
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="action-btn"
+                      onClick={() => DeleteData(index)}
+                    >
+                      Deletar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
+
+          {showPopUpAdd && (
+            <div className="popup-overlay">
+              <div className="popup-edit">
+                <h2>Editar</h2>
+                <div className="inputt">
+                  <label>Critérios de avaliação</label>
+                  <input
+                    type="text"
+                    value={textInput}
+                    onChange={(e) => setTextInput(e.target.value)}
+                  />
+                </div>
+                <div className="select-pop">
+                  <label>Crítico (C) ou Desejável (D)</label>
+                  <select
+                    value={selectedOption}
+                    onChange={(e) => setSelectedOption(e.target.value)}
+                  >
+                    <option value="C">Crítico (C)</option>
+                    <option value="D">Desejável (D)</option>
+                  </select>
+                </div>
+                <div className="edit-btns">
+                  <button onClick={SaveEdit}>Salvar alterações</button>
+                  <button onClick={() => setShowPopUpAdd(false)}>
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="planneraulas">
+          <h3 className="title-section">Planejamento de aulas</h3>
+          <label>Datas e conhecimentos abordados</label>
+          <button
+            className="add-plan"
+            onClick={() => {
+              setShowPopUpPlan(true);
+            }}
+          >
+            <Plus width={25} height={25} strokeWidth={2} />
+          </button>
+
+          {showPopUpPlan && (
+            <div className="popup-overlay">
+              <div className="popup-plan">
+                <h2>Planejamento de aulas</h2>
+                <h3>Descreva o conteúdo das aulas e selecione os conhecimentos, recursos e estratégias a serem desenvolvidas.
+                </h3>
+                <div className="dates">
+                  <div className="proposta">
+                    <label>Data proposta</label>
+                    <input type="date" name="" id="" />
+                  </div>
+                  <div className="final">
+                    <label>Data final</label>
+                    <input type="date" name="" id="" />
+                  </div>
+                </div>
+                <Multiselect
+                  options={options}
+                  value={value4}
+                  onChange={(o) => setValue4(o)}
+                  multiple={true}
+                />
+                <Multiselect
+                  options={options}
+                  value={value5}
+                  onChange={(o) => setValue5(o)}
+                  multiple={true}
+                />
+                <Multiselect
+                  options={options}
+                  value={value6}
+                  onChange={(o) => setValue6(o)}
+                  multiple={true}
+                />
+                <div className="edit-btns">
+                  <button>Salvar</button>
+                  <button onClick={() => setShowPopUpPlan(false)}>
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </main>
