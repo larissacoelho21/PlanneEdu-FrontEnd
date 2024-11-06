f0c27ea868d69b01c57b950548c29acbc405f3e0
Sarah Cruz <sarinha.studiesllz@gmail.com>
2024-11-06 11:26:01 -0300

    Início da construção da página de criação de plano de curso (OPP)

--- PlanneEdu_Front/src/Pages/Opp/AddPlanoCursoOpp.tsx
+++ PlanneEdu_Front/src/Pages/Opp/AddPlanoCursoOpp.tsx
@@ -1,21 +1,90 @@
+import { useState } from "react";
 import { AddMateria } from "../../Components/PlanoDeCurso/AddMateria";
 import { Competencias } from "../../Components/PlanoDeCurso/Competencias";
+import { SubNavbar } from "../../Components/SubNavbar/SubNavbar";
+import "../../Css/Opp/AddPlanoCurso.css"
 
 export function AddPlanoCurso() {
-  
+  interface InputFieldProps {
+    id: string;
+    label: string;
+    type?: string;
+    value?: string;
+    onChange?: (
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => void;
+  }
+
+  function InputField({
+    id,
+    label,
+    type = "text",
+    value,
+    onChange,
+  }: InputFieldProps) {
+    const [isFilled, setIsFilled] = useState(!!value);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setIsFilled(event.target.value !== "");
+      if (onChange) onChange(event);
+    };
+
+    return (
+      <fieldset className={`Fieldset ${isFilled ? "filled" : ""}`}>
+        <label className="label-add" htmlFor={id}>
+          {label}
+        </label>
+        <input
+          className="input-add"
+          id={id}
+          type={type}
+          value={value}
+          onChange={handleInputChange}
+          autoComplete="off"
+        />
+      </fieldset>
+    );
+  }
 
   return (
     <section className="AddPlanCourses">
+      <SubNavbar />
       <div className="AddInfo">
-        <form>
+        <div className="intro">
+          <h1>Crie um plano de curso</h1>
+          <h2>Preencha as informações corretamente</h2>
+        </div>
+
+        <form className="form-course">
           <div className="inputs-opp">
-            <input type="text" placeholder="Nome do Curso" />
-            <input type="text" placeholder="Categoria" />
-            <input type="text" placeholder="Objetivo" />
-            <input type="text" placeholder="Competências Profissionais" />
-            <input type="text" placeholder="Carga horária" />
-            <input type="text" placeholder="Qtd de semestres" />
-            <input type="text" placeholder="Tempo de curso" />
+            <div className="input-f">
+              <InputField label="Nome do Curso" type="text" id="nome-course" />
+            </div>
+            <div className="input-f">
+              <InputField label="Categoria" type="text" id="category-course" />
+            </div>
+            <div className="input-f">
+              <InputField label="Objetivo" type="text" id="obj-course" />
+            </div>
+            <div className="input-f">
+              <InputField label="Competências" type="text" id="compt-course" />
+            </div>
+            <div className="input-row">
+              <div className="cargah">
+                <InputField
+                  label="Carga Horária"
+                  type="text"
+                  id="cargah-course"
+                />
+              </div>
+              <div className="qtd-semestre">
+                <InputField
+                  label="Quantidade de semestres"
+                  type="number"
+                  id="semestres-course"
+                />
+              </div>
+            </div>
           </div>
 
           <div className="grade-horaria">
